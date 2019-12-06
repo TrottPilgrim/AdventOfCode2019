d8e2c9ce095c888f565b2ee6f76f428c8cc16666
Thomas Lim <tl1860@nyu.edu>
2019-12-05 21:50:11 -0500

    I will try to go back and clean this up on the weekend, but day 5 is done

--- Day5/Day5.js
+++ Day5/Day5.js
@@ -11,11 +11,14 @@ fs.readFile('./input', 'utf8', (err, data) => {
 });
 
 function runOps(arr) {
+  let counter = 0;
   for (let i = 0; i < arr.length; ) {
+    counter += 1;
+    // console.log(counter, arr);
     const instr = `${arr[i]}`;
 
     const opcode = parseInt(instr.slice(-2));
-    // console.log(opcode);
+    // console.log('op:', opcode, i);
     const modes = instr
       .slice(0, -2)
       .split('')
@@ -41,15 +44,53 @@ function runOps(arr) {
       i += 4;
     } else if (opcode === 3) {
       const dest = modes[0] ? i + 1 : arr[i + 1];
-      arr[dest] = 1;
+      arr[dest] = 5;
       i += 2;
     } else if (opcode === 4) {
       const dest = modes[0] ? i + 1 : arr[i + 1];
       console.log(arr[dest]);
       i += 2;
+    } else if (opcode === 5) {
+      const arg1 = arr[modes[0] ? i + 1 : arr[i + 1]];
+      const arg2 = arr[modes[1] ? i + 2 : arr[i + 2]];
+      if (arg1 !== 0) {
+        i = arg2;
+      } else {
+        i += 3;
+      }
+    } else if (opcode === 6) {
+      const arg1 = arr[modes[0] ? i + 1 : arr[i + 1]];
+      const arg2 = arr[modes[1] ? i + 2 : arr[i + 2]];
+      if (arg1 === 0) {
+        i = arg2;
+      } else {
+        i += 3;
+      }
+    } else if (opcode === 7) {
+      const arg1 = arr[modes[0] ? i + 1 : arr[i + 1]];
+      const arg2 = arr[modes[1] ? i + 2 : arr[i + 2]];
+      const dest = modes[2] ? i + 3 : arr[i + 3];
+      if (arg1 < arg2) {
+        arr[dest] = 1;
+      } else {
+        arr[dest] = 0;
+      }
+      i += 4;
+    } else if (opcode === 8) {
+      const arg1 = arr[modes[0] ? i + 1 : arr[i + 1]];
+      const arg2 = arr[modes[1] ? i + 2 : arr[i + 2]];
+      const dest = modes[2] ? i + 3 : arr[i + 3];
+      if (arg1 === arg2) {
+        arr[dest] = 1;
+      } else {
+        arr[dest] = 0;
+      }
+      i += 4;
     } else {
       console.error('Encountered a non-valid opcode!');
       return;
     }
   }
 }
+
+// runOps('3,9,8,9,10,9,4,9,99,-1,8'.split(',').map(e => parseInt(e)));
